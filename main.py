427523fc0b0975091efb626e42c6d5168f7ab1f7
Goh Zhao Yang <gzy5965@gmail.com>
2019-04-23 09:21:44 +0000

    Added new function called name

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flasgger import Swagger
-
+import pandas as pd
+import pymysql
 from config.swagger import swagger_template, swagger_config
 from controllers import example_controller
 
 app = Flask(__name__)
 app.config['SWAGGER'] = {
-    'title': 'YOUR TITLE HERE',
+    'title': '',
     'uiversion': 3
 }
 
 swagger = Swagger(app, template=swagger_template, config=swagger_config)
 
-action_options = {
-    "testing": example_controller.example_do_something,
-}
+@app.route('/name')
+def name():
+    return jsonify({"name":"gzy"})
 
-
-@app.route('/')
+@app.route('/hello_world')
 def hello_world():
     '''
     file: documentations/example.yml
     '''
-    return example_controller.example_do_something()
+    sql = """
+    SELECT * FROM `bigquery-public-data.samples.gsod` LIMIT 100
+    """
+    df = pd.read_gbq(sql, project_id='radiant-micron-790',  dialect='standard')
+    records = df.to_dict(orient='records')
+    return jsonify({"results": records})
 
-
-# This is specifically for chatbot
-@app.route('/chatbot_entrypoint', methods=['POST'])
-def chatbot_entrypoint():
+@app.route('/call_query')
+def call_query():
     '''
-    file: documentations/chatbot_entrypoint.yml
+    file: documentations/example.yml
     '''
-    req = request.get_json(silent=True)
-    query = request.get_json(silent=True)['queryResult']
-    action = query['action']
-    return action_options[action](query, req)
-
+    sql = """
+    SELECT * FROM `bigquery-public-data.samples.gsod` LIMIT 100
+    """
+    conn = pymysql.connect(host='35.186.156.137',
+                             user='viewer',
+                             password='password123',
+                             db='datadict',
+                             cursorclass=pymysql.cursors.DictCursor)
+    df = pd.read_sql('SELECT * FROM datadict.systems',con=conn)
+    records = df.to_dict(orient='records')
+    return jsonify({"results": records})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
